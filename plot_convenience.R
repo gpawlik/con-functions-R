d7c12d1dd74cc83288c763acfcd0436765895f9d
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-17 18:36:54 +1000

    Add comments to shade_between() and shade_outside()

--- plot_convenience.R
+++ plot_convenience.R
@@ -10,8 +10,8 @@ shade_between <- function(x,y, lower, upper, shade.col="red", shade.border=NULL,
     #===========================================================================
     #                                                              SHADE BETWEEN
     #===========================================================================
-    # Creates a plot which shades the area under the curve between points lower 
-    # and upper. 
+    # Creates a plot which shades the area under the curve between "lower" and 
+    # "upper" points along x. 
     # 
     # Note, that depending on the resolution of the x points, you may not 
     # actually get the shaded region in the exact upper and lower points you 
@@ -56,29 +56,35 @@ shade_between <- function(x,y, lower, upper, shade.col="red", shade.border=NULL,
     #
     #===========================================================================
     
-    #---------------------------------------------------------------------------
-    #                                                                      Setup
-    #---------------------------------------------------------------------------
-    tolerance = tol=.Machine$double.eps ^ 0.5
+    #-------------------------------------------------------------------------
+    #                                                                    Setup
+    #-------------------------------------------------------------------------
+    tolerance = tol=.Machine$double.eps ^ 0.5  # For floating point comparisons
     
-    # Create points for the shaded region
+    #-------------------------------------------------------------------------
+    #                                      Create points for the shaded region
+    #-------------------------------------------------------------------------
+    # look for values in x between "lower" and "upper" 
     x2 = c(x[x+tolerance>=lower & x<=upper+tolerance])
-    x2 = c(x2[1], x2, x2[length(x2)])
+    x2 = c(x2[1], x2, x2[length(x2)])   # Adds end points
     
+    # Look for subset of points in y that correspont to the region of interest
     lower_index = find.matches(x2[2], x, tol=tolerance, maxmatch=1)$matches[[1]]
     upper_index = find.matches(x2[length(x2)-1], x, tol=tolerance, maxmatch=1)$matches[[1]]
     y2 = c(0, y[lower_index: upper_index],0)
     
     
-    #---------------------------------------------------------------------------
-    #                                                                    Plot it
-    #---------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    #                                                                  Plot it
+    #-------------------------------------------------------------------------
+    #preliminary plot to get the axes drawn
     plot(x,y, ...)
     
+    # The sahded region
     polygon(x2,y2,col=shade.col, border=shade.border, lty=shade.lty, 
             density=shade.density, angle=shade.angle)
     
-    # Draw over the shaded area border.
+    # Now draw the actual plot to draw over the shaded area.
     points(x,y, ...)    
 }
 
@@ -91,8 +97,8 @@ shade_outside <- function(x,y, lower, upper, shade.col="red", shade.border=NULL,
     #                                                              SHADE BETWEEN
     #===========================================================================
     # Creates a plot which shades the area under the curve of the region outside 
-    # lower and upper. SO for example it can be used to shade the tails of a 
-    # bell curve. 
+    # "lower" and "upper" along x. So for example it can be used to shade the 
+    # tails of a bell curve. 
     # 
     # Note, that depending on the resolution of the x points, you may not 
     # actually get the shaded region in the exact upper and lower points you 
@@ -137,38 +143,49 @@ shade_outside <- function(x,y, lower, upper, shade.col="red", shade.border=NULL,
     #
     #===========================================================================
     
-    #---------------------------------------------------------------------------
-    #                                                                      Setup
-    #---------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    #                                                                    Setup
+    #-------------------------------------------------------------------------
     
-    tolerance = tol=.Machine$double.eps ^ 0.5
-    # Create points for the left hand shaded region
-    x2 = x[x<=lower+tolerance]
-    x2 = c(x2[1], x2, x2[length(x2)]) 
-        
+    tolerance = tol=.Machine$double.eps ^ 0.5 # For floating point comparisons
+    
+    #-------------------------------------------------------------------------
+    #                            Create points for the left hand shaded region
+    #-------------------------------------------------------------------------
+    # Left hand portion looks for values in x less or equal to "lower" 
+    x2 = x[x<=lower+tolerance]            
+    x2 = c(x2[1], x2, x2[length(x2)])   # Add end points
+    
+    # Find subset of values in y that correspond to left hand region
     lower_index = find.matches(x2[length(x2)], x, tol=tolerance, maxmatch=1)$matches[[1]]
-    y2 = c(0, y[1: lower_index],0)
+    y2 = c(0, y[1: lower_index],0)      # Add end points
     
     
-    # Create points for the right hand shaded region
+    #-------------------------------------------------------------------------
+    #                            Create points for the right hand shaded region
+    #-------------------------------------------------------------------------
+    # Right hand portion looks for values in x greater or equal to "upper" 
     x3 = x[x+tolerance>=upper]
-    x3 = c(x3[1], x3, x3[length(x3)])
+    x3 = c(x3[1], x3, x3[length(x3)])       # Add end points
     
+    # Find subset of values in y that correspond to right hand region
     upper_index = find.matches(x3[2], x, tol=tolerance, maxmatch=1)$matches[[1]]
-    y3 = c(0, y[upper_index: length(x)],0)
+    y3 = c(0, y[upper_index: length(x)],0)  # Add end points
     
         
-    #---------------------------------------------------------------------------
-    #                                                                    Plot it
-    #---------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+    #                                                                  Plot it
+    #-------------------------------------------------------------------------
+    #preliminary plot to get the axes drawn
     plot(x,y, ...)
     
+    # Shaded Region
     polygon(x2,y2,col=shade.col, border=shade.border, lty=shade.lty, 
             density=shade.density, angle=shade.angle)
     polygon(x3,y3,col=shade.col, border=shade.border, lty=shade.lty, 
             density=shade.density, angle=shade.angle)
     
-    # Draw over the shaded area border.
+    # Now draw the actual graph to pain over the shaded area border.
     points(x,y, ...)    
 }
 
