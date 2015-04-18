bee7db1d32eec53672a5f7a1b8f86e16046401ad
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-18 15:47:43 +1000

    Add comment of known bug to both shade_after() and shade_before(). The bug is that it only plots with dots, lines not appearing when specified.

--- plot_convenience.R
+++ plot_convenience.R
@@ -5,12 +5,14 @@ library(Hmisc)  # Used for floating point matches
 
 shade_after <- function(x, y, boundary, ...){
     # Shades area under the curve from boundary to values of x that are higher.
+    # TODO: BUG: Only printing graph with dots, specifying lines not working
     upper <- x[length(x)]
     shade_between(x, y, boundary, upper)
 }
 
 shade_before <- function(x, y, boundary, ...){
     # Shades area under the curve from boundary to values of x that are lower.
+    # TODO: BUG: Only printing graph with dots, specifying lines not working
     lower <- x[1]
     shade_between(x, y, lower, boundary)
 }
