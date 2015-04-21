c2b29d2b9a5ef1f97ece908a4406ceba6574ee6d
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-21 11:37:10 +1000

    Add stat_convenience.R with a definition of confidence_interval(). But the confidence_interval() function has not been implemented yet.

---
+++ stat_convenience.R
@@ -0,0 +1,12 @@
+
+confidence_interval <- function(x, conf.p=NA, conf.range=NA){
+    # calcualtes the confidence interval
+    # If provide conf.p = 0.95 you get the values that the population is likely 
+    # to be with a 95% confidence.
+    # If you provide conf.range = (12,17) You get a percentage telling you what 
+    # the likelihood of the population being within that range is.
+    print("TODO: implement confidence_interval()")
+    
+}
+
+#source("~/programming/R/projects/convenience/stat_convenience.R")
