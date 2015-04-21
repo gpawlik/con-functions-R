6e0ce0c7d6fa330e42d140f1a9f62c970754ff3c
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-21 19:17:59 +1000

    Add workflow_convenience.R. Add definition for getFromWeb(), but have not implemented it yet.

---
+++ workflow_convenience.R
@@ -0,0 +1,14 @@
+getFromWeb <- function(url, local.dir, save.as-NA, cache=TRUE, unzip=FALSE, 
+                       unzip.path=NA, delete.zip=FALSE){
+    # Get a file from the internet and save it to local.dir 
+    # You can rename the file to something more meaningful by setting save.as 
+    # to a "myFile.csv" for instance. 
+    # Caching stops the need to have to download the file everytime you run the 
+    # script.
+    # If you are downloading a zip you can get it to unzip the file 
+    # automatically, you can specify that the contents be extracted in a 
+    # separate directory using unzip.path. 
+    # you can also specify that the zip file be deleted, so you can keep just 
+    # the extracted contents. 
+    
+}
