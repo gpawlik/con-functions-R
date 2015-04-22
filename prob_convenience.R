43928acdf1dd5c5d5f7bdd86829b446ad0744dfc
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-04-22 17:15:32 +1000

    Add file prob_convenience.R Add function coinflipper() to prob_convenience.R

---
+++ prob_convenience.R
@@ -0,0 +1,28 @@
+source("~/programming/R/projects/convenience/plot_convenience.R")
+source("~/programming/R/projects/convenience/fancyprint.R")
+
+coinflipper <- function(n.flips=1, n.success=1, p=0.5){
+    # Flips a coin multiple times, and it:
+    # - plots a distribution plot for n number of flips
+    # - tells you how many possible ways there are to get exactly n successes
+    # - tells you probability of getting exactly n number of successes.
+    plot_distribution(dist="binomial", n=n.flips, p=p)
+    total.outcomes = 2^n.flips
+    p.satisfy = dbinom(3, size=5, p=0.5)
+    n.satisfy = p.satisfy * total.outcomes
+    
+    #TODO: include print out of getting n or more, n or less successes. 
+    fill = 60
+    print("===================================================================")
+    print(sprintf("Flipping a coin %d times with p=%0.2f", n.flips, p))
+    print("===================================================================")
+    printkv("Total number of possibilities : ", total.outcomes, sep=" ", fill=fill, fill_char="_")
+    print("")
+    printkv(sprintf("Possible ways of getting exactly %d successes: ", n.success), n.satisfy, sep=" ", fill=fill, fill_char="_")
+    printkv(sprintf("Probability of getting exactly %d successes: ", n.success), p.satisfy, sep=" ", fill=fill, fill_char="_")
+    print("")
+    print("===================================================================")
+}
+
+#coinflipper(n.flips=9, n.success=2, p=0.5)
+
