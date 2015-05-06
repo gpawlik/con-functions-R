cc8d3b716c3d46453bbd7e16b458b435c500f82c
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-05-06 19:11:05 +1000

    Removed feature for plotting coin flips simulation - There was a circularity in the dependency to plot.convenience - Added a TODO: to figure a way around this

--- stat.convenience/R/prob_convenience.R
+++ stat.convenience/R/prob_convenience.R
@@ -1,4 +1,4 @@
-library("plot.convenience")
+#library("plot.convenience")
 library("fancyprint")
 
 #' coinflipper
@@ -15,7 +15,9 @@ coinflipper <- function(n.flips=1, n.success=1, p=0.5){
     # - plots a distribution plot for n number of flips
     # - tells you how many possible ways there are to get exactly n successes
     # - tells you probability of getting exactly n number of successes.
-    plot_distribution(dist="binomial", n=n.flips, p=p)
+    # TODO: I had to comment out the call to plot distribution to avoid 
+    #       circularities in dependencies. Find a way to deal with this.
+    #plot_distribution(dist="binomial", n=n.flips, p=p)
     total.outcomes = 2^n.flips
     p.satisfy = dbinom(3, size=5, p=0.5)
     n.satisfy = p.satisfy * total.outcomes
