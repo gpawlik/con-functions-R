97f214c5ddfefe3b7ced6d2104e6bd2e6a388f7b
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-05-13 21:43:36 +1000

    Add ct()

--- stat.convenience/R/confidence_intervals.R
+++ stat.convenience/R/confidence_intervals.R
@@ -45,3 +45,53 @@ cbinom <- function(size=1, prob=0.5, type="equal", conf=0.95){
     return(c(cutoff_lower, cutoff_upper))
 }
 
+
+
+# ==============================================================================
+#                                                                             CT
+# ==============================================================================
+#' ct
+#' 
+#' Confidence region for a T Distribution. 
+#' 
+#' @param df (int) degrees of freedom.
+#' @param type string. type of hypothesis test taken. 
+#'        
+#'        - for two tailed test use "equal" (Default)  or "="
+#'        
+#'        - for one-tailed lower tail test use "less" or "<" 
+#'        
+#'        - for one-tailed upper tail test use "more" or "greater" or "higher" 
+#'          or ">" 
+#' @param conf confidence interval used
+#' @return a vector with two values representing the lower and upper points that 
+#'         fit within your confidence interval.
+#' @examples
+#' ct(df=20, type="equal", conf=0.99)
+#' ct(df=20, type="=", conf=0.90)
+#' ct(df=20, type="less", conf=0.95)
+#' ct(df=20, type="<", conf=0.90)
+#' ct(df=20, type="more", conf=0.90)
+#' ct(df=20, type=">", conf=0.90)
+#' @export
+ct <- function(df=10, type="equal", conf=0.95){
+    # Account for the different types of cutoff quantiles
+    alpha = 1 - conf
+    if ((type == "less") | (type == "<")){
+        p_lower = alpha
+        p_upper = 1.0
+    } else if ((type == "more") | (type == "greater") | (type == "higher") 
+               | (type == ">")){
+        p_lower = 0.0
+        p_upper = conf
+    } else if ((type == "equal") | (type == "=")){
+        p_lower= alpha/2
+        p_upper = 1 - (alpha/2)
+    }
+    
+    # calculate the cutoff points
+    cutoff_lower = qt(p_lower, df=df, lower.tail=TRUE)
+    cutoff_upper = qt(p_upper, df=df, lower.tail=TRUE)
+    
+    return(c(cutoff_lower, cutoff_upper))
+}
