297153037cf752ab698884750f61cfee60ab3f05
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-06-13 15:44:49 +1000

    Rename .set.grid.parameters() to set_par_for_n_subplots()

--- plot.convenience/R/plot.cols.R
+++ plot.convenience/R/plot.cols.R
@@ -156,7 +156,7 @@ plot.cols <-function(x, y=NA, type="auto", labelCex=1, col="darkgray",
         x = data.frame(x)
     }
     # Set New global plotting settings for a grid layout
-    .set.grid.parameters(ncols)
+    set_par_for_n_subplots(ncols)
     
     #--------------------------------------------------------------------------
     #                                            Set up Gradient Color Settings
@@ -250,9 +250,8 @@ plot.cols <-function(x, y=NA, type="auto", labelCex=1, col="darkgray",
 ################################################################################
 #                                                               HELPER FUNCTIONS
 ################################################################################
-
 #===============================================================================
-#                                                           .SET.GRID.PARAMETERS
+#                                                         SET_PAR_FOR_N_SUBPLOTS
 #===============================================================================
 #' Helper function to set global parameters of par() that are appropriate for 
 #' plotting n number of cells. 
@@ -260,7 +259,7 @@ plot.cols <-function(x, y=NA, type="auto", labelCex=1, col="darkgray",
 #' You secify n, and it automatically calculates what size the grid, and other 
 #' plotting parameters to generate a nice layout.  
 #===============================================================================
-.set.grid.parameters <- function(n){
+set_par_for_n_subplots <- function(n){
     # Calculate number of cells per row/column to plot
     if (n == 1){
         ngridRows = 1
@@ -276,3 +275,6 @@ plot.cols <-function(x, y=NA, type="auto", labelCex=1, col="darkgray",
         mgp = c(1.5, 0.3, 0), tck = -0.01, oma=c(1, 1, 0.5, 0.5))
 }
 
+# DEFAULTS
+#par(mfrow=c(1,1), mar=c(5.1, 4.1, 4.1, 2.1), 
+#            mgp = c(3, 1, 0), tck = NA, oma=c(0, 0, 0, 0))
